e8aab39b63afc1d1dc0d10687e15d17a41f70920
konyka <konyka@126.com>
2019-03-31 22:25:09 +0800

    add enum mm_coroutine_status

--- src/mm_coroutine.h
+++ src/mm_coroutine.h
@@ -18,7 +18,50 @@
 #ifndef __DARKBLUE_MM_COROUTINE_H__
 #define __DARKBLUE_MM_COROUTINE_H__
 
+
+
+#define MM_CO_MAX_EVENT_SIZE    (1024*1024)
+#define MM_CO_MAX_STACK_SIZE    (16*1024)
+
+
+
+typedef void (*p_co_routine)(void *);
+
+typedef enum {
+
+    MM_COROUTINE_STATUS_BIRTH, 
+
+    MM_COROUTINE_STATUS_READY,
+    MM_COROUTINE_STATUS_SLEEPING,
+
+    MM_COROUTINE_STATUS_WAIT_READ,
+    MM_COROUTINE_STATUS_WAIT_WRITE,
+
+    MM_COROUTINE_STATUS_WAIT_IO_READ,
+    MM_COROUTINE_STATUS_WAIT_IO_WRITE,
+
+    MM_COROUTINE_STATUS_BUSY,
+    
+    MM_COROUTINE_STATUS_EXPIRED,
+    
+    MM_COROUTINE_STATUS_EXITED,
+
+    MM_COROUTINE_STATUS_PENDING_RUNCOMPUTE,
+    MM_COROUTINE_STATUS_RUNCOMPUTE,
+      
+    
+    MM_COROUTINE_STATUS_FDEOF,
+    MM_COROUTINE_STATUS_DETACH,
+    MM_COROUTINE_STATUS_CANCELLED,
+
+    MM_COROUTINE_STATUS_WAIT_MULTI,
+
+} mm_coroutine_status;
+
+
+
 typedef struct _mm_cpu_ctx {
+
     void *rsp; //
     void *rbp;
     void *rip;
@@ -30,7 +73,9 @@ typedef struct _mm_cpu_ctx {
     void *r3;
     void *r4;
     void *r5;
+
 } mm_cpu_ctx;
+
 
 
 
