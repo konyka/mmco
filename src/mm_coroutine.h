cbe1a38db1265bc2d794e20395b86d4331f81a34
konyka <konyka@126.com>
2019-03-31 20:57:22 +0800

    add struct _mm_cpu_ctx

---
+++ src/mm_coroutine.h
@@ -0,0 +1,54 @@
+/**
+ *===========================================================================
+ *  DarkBlue Engine Source File.
+ *  Copyright (C), DarkBlue Studios.
+ * -------------------------------------------------------------------------
+ *    File name: mm_coroutine.h
+ *      Version: v0.0.0
+ *   Created on: Mar 31, 2019 by konyka
+ *       Editor: Sublime Text3
+ *  Description: 
+ * -------------------------------------------------------------------------
+ *      History:
+ *
+ *===========================================================================
+ */
+ 
+
+#ifndef __DARKBLUE_MM_COROUTINE_H__
+#define __DARKBLUE_MM_COROUTINE_H__
+
+typedef struct _mm_cpu_ctx {
+    void *rsp; //
+    void *rbp;
+    void *rip;
+    void *rdi;
+    void *rsi;
+    void *rbx;
+    void *r1;
+    void *r2;
+    void *r3;
+    void *r4;
+    void *r5;
+} mm_cpu_ctx;
+
+
+
+
+#endif /* __DARKBLUE_MM_COROUTINE_H__ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
